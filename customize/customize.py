9eacf0d61f76401da32ae8ab4a012daf2e76f05d
tao <1483906080@qq.com>
2019-11-27 18:56:28 +0800

    init start

---
+++ customize/customize.py
@@ -0,0 +1,13 @@
+from indicator import interface
+import numpy as np
+
+
+class Customize:
+    """
+    自定义方法类
+    """
+    def __init__(self):
+        pass
+
+    def method(self):
+        pass
